65b35d03e59fa509fba16174b4f4df1bc7e6d48c
Filipe Linhares <lip.linhares@gmail.com>
2019-09-13 04:03:45 -0300

    Add replace function with change-case and chalk

--- src/generator.js
+++ src/generator.js
@@ -1,10 +1,16 @@
 const fs = require("fs-extra");
 const path = require("path");
 const walkSync = require("walk-sync");
+const caase = require('change-case');
+const chalk = require('chalk');
+const package = require('../package.json');
+
+const { findAll } = require('./util.js');
+
 const ARGS = process.argv;
 
 if (ARGS.length < 4) {
-  console.log("faz direito po");
+  console.log(chalk.red("Wrong number of arguments"))
   process.exit(1);
 }
 
@@ -12,10 +18,10 @@ const folder = ARGS[2];
 const param = ARGS[3];
 const dest = ARGS[4];
 
+const REGEX_CASES = /%camel%|%constant%|%lower%|%lcFirst%|%no%|%kebab%|%pascal%|%path%|%sentence%|%snake%|%swap%|%title%|%upper%|%ucFirst%/g
+
 const templatesFolder = path.resolve(__dirname, `../generator`, folder);
 const tmpFolder = path.resolve(__dirname, `../.tmp-generator`, folder);
-
-const findAll = search => new RegExp(search, "g");
 
 if (fs.pathExistsSync("./.tmp-generator")) {
   fs.removeSync("./.tmp-generator");
@@ -27,20 +33,27 @@ fs.copySync(templatesFolder, tmpFolder);
 
 const paths = walkSync(tmpFolder);
 
+console.log(walkSync(tmpFolder, {ignore: ['files']}))
+
 paths.forEach(file => {
   const fileLocation = path.join(tmpFolder, file);
   const fileStat = fs.statSync(fileLocation);
 
   if (fileStat.isFile()) {
     const x = fs.readFileSync(fileLocation, "utf8");
-    const l = x.replace(findAll("%%%"), param);
+    const l = x.replace(findAll(REGEX_CASES), (match, param) => {
+      return caase[match.replace(/%/g, '')](param);
+    });
 
-    const newFileName = fileLocation.replace(findAll("%%%"), param);
+    const newFileName = fileLocation.replace(findAll(REGEX_CASES), (match, param) => {
+      return caase[match.replace(/%/g, '')](param);
+    });
+
     fs.renameSync(fileLocation, newFileName);
-    console.log(newFileName);
     fs.writeFileSync(newFileName, l);
   }
 });
 
-fs.copySync(path.join(tmpFolder), dest);
+fs.copySync(tmpFolder, dest);
+
 fs.remove("./.tmp-generator");
---
+++ src/util.js
@@ -0,0 +1,5 @@
+const findAll = search => new RegExp(search, "g");
+
+module.exports = {
+  findAll
+};
