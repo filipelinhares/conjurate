513272014dcce1b4dea8c8f93a722ebd4d668052
Filipe Linhares <lip.linhares@gmail.com>
2019-12-02 00:02:52 -0300

    Update help message and CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,24 @@
 ## [0.4.0]
 
 Start using `vinyl-fs` instead of a temp dir.
+
+### Added:
+
+- `--logs` and `--no-logs` flags.
+
+### Changed:
+
+- Placeholders names to:
+  - `%camel-case%`
+  - `%lower-case%`
+  - `%no-case%`
+  - `%dot-case%`
+  - `%dash-case%`
+  - `%pascal-case%`
+  - `%path-case%`
+  - `%snake-case%`
+  - `%swap-case%`
+  - `%title-case%`
+  - `%upper-case%`
+  - `%first-word%`
+  - `%last-word%`
--- src/content.js
+++ src/content.js
@@ -1,48 +1,57 @@
-const chalk = require('chalk');
-const conjuratePkg = require('../package.json');
+const chalk = require("chalk");
+const conjuratePkg = require("../package.json");
 
 const VERSION = conjuratePkg.version;
 
-const printCommands = templates => (
+const printCommands = templates =>
   `
-  ${chalk.italic.gray('Your templates:')}
-${Object.keys(templates).map(t => `  • ${chalk.bold(t)} ⇢ ${chalk.dim('--output:')} ${chalk.italic.gray(templates[t])}`).join('\n')}
-`
-)
+  ${chalk.italic.gray("Your templates:")}
+${Object.keys(templates)
+  .map(
+    t =>
+      `  • ${chalk.bold(t)} ⇢ ${chalk.dim("--output:")} ${chalk.italic.gray(
+        templates[t]
+      )}`
+  )
+  .join("\n")}
+`;
 
 const HELP = `
-  ${chalk.bold('Conjurate')}@${VERSION} - ${chalk.italic('Easy scaffolding tool')}
-  
-  ${chalk.italic.gray('Create config file or add to package.json:')}
+  ${chalk.bold("Conjurate")}@${VERSION} - ${chalk.italic(
+  "Easy scaffolding tool"
+)}
+
+  ${chalk.italic.gray("Create config file or add to package.json:")}
     conjurate --init
 
-  ${chalk.italic.gray('Usage:')}
+  ${chalk.italic.gray("Usage:")}
     conjurate <template-name> <placeholder-name>
 
-  ${chalk.italic.gray('Options:')}
-    -o, --out        overwrite the default destination dir for choose template.
-    -v, --version    show version number
-    -h, --help       show this message
-    -t, --templates  print templates listed in your conjurate config
+  ${chalk.italic.gray("Options:")}
+    -o, --output      overwrite the default destination dir for choose template.
+    -v, --version     show version number
+    -h, --help        show this message
+    -t, --templates   print templates listed in your conjurate config
+    -l, --logs        print template generation info
+    -no-l, --no-logs  do not print template generation info
 
-  ${chalk.italic.gray('Docs:')}
+  ${chalk.italic.gray("Docs:")}
     https://github.com/filipelinhares/conjurate
     https://conjurate.surge.sh
 `;
 
-const CONJURATE_CONFIG_JSON =
-`{
+const CONJURATE_CONFIG_JSON = `{
   "templatesRoot": "./conjurate",
   "templates": {
     "<template-name>": "./<default-destination-dir>",
     "<example-component>": "<example-./src/components>"
   }
 }
-`
+`;
 
 module.exports = {
   CONJURATE_CONFIG_JSON,
   HELP,
   VERSION,
-  printCommands,
-}
+  printCommands
+};
