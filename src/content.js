af8dc130875608b1c4b9179e72d4e89d9f73de69
Filipe Linhares <lip.linhares@gmail.com>
2019-12-14 01:05:01 -0300

    Remove chalk as dependency

--- src/content.js
+++ src/content.js
@@ -1,56 +1,48 @@
-const chalk = require('chalk')
+const gray = require('ansi-gray')
+const dim = require('ansi-dim')
+const italic = require('ansi-italic')
+const bold = require('ansi-bold')
 const conjuratePkg = require('../package.json')
 
 const VERSION = conjuratePkg.version
 
 const printCommands = templates =>
   `
-  ${chalk.italic.gray('Your templates:')}
+  ${italic(gray('Your templates:'))}
 ${Object.keys(templates)
   .map(
     t =>
-      `  • ${chalk.bold(t)} ⇢ ${chalk.dim('--output:')} ${chalk.italic.gray(
+      `  • ${bold(t)} ⇢ ${dim('--output:')} ${italic(gray(
         templates[t]
-      )}`
+      ))}`
   )
   .join('\n')}
 `
 
 const HELP = `
-  ${chalk.bold('Conjurate')}@${VERSION} - ${chalk.italic(
+  ${bold('Conjurate')}@${VERSION} - ${italic(
   'Easy scaffolding tool'
 )}
 
-  ${chalk.italic.gray('Create config file or add to package.json:')}
+  ${italic(gray('Create config file or add to package.json:'))}
     conjurate --init
 
-  ${chalk.italic.gray('Usage:')}
+  ${italic(gray('Usage:'))}
     conjurate <template-name> <placeholder-name>
 
-  ${chalk.italic.gray('Options:')}
-    -o, --output      overwrite the default destination dir for choose template.
-    -v, --version     show version number
-    -h, --help        show this message
+  ${italic(gray('Options:'))}
     -t, --templates   print templates listed in your conjurate config
-    -l, --logs        print template generation info
+    -o, --output      overwrite the default destination dir for choose template
+    -h, --help        show this message
+    -v, --version     show version number
     -no-l, --no-logs  do not print template generation info
 
-  ${chalk.italic.gray('Docs:')}
+  ${italic(gray('Docs:'))}
     https://github.com/filipelinhares/conjurate
     https://conjurate.surge.sh
 `
 
-const CONJURATE_CONFIG_JSON = `{
-  "templatesRoot": "./conjurate",
-  "templates": {
-    "<template-name>": "./<default-destination-dir>",
-    "<example-component>": "<example-./src/components>"
-  }
-}
-`
-
 module.exports = {
-  CONJURATE_CONFIG_JSON,
   HELP,
   VERSION,
   printCommands
